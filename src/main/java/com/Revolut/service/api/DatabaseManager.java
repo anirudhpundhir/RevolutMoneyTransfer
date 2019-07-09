a75c510dc28b3199403940063f0d970a943165f2
ANIRUDH <NDH00234@Nmcorp.Nissan.Biz>
2019-07-10 00:43:35 +0530

    Issue resolved

--- src/main/java/com/Revolut/service/api/DatabaseManager.java
+++
@@ -1,8 +0,0 @@
-package com.Revolut.service.api;
-
-import org.jooq.DSLContext;
-
-public interface DatabaseManager {
-    DSLContext getSqlDSL();
-    void close();
-}
--- src/main/java/com/Revolut/service/sample.java
+++
@@ -1,4 +0,0 @@
-package com.Revolut.service;
-
-public class sample {
-}
